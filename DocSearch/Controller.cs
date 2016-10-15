1a6372ac05493284e93261f16925d86e42b722a4
Jan Wielebiński <jan.wielebinski@student.put.poznan.pl>
2016-10-15 11:26:47 +0200

    Initial commit
    Stemmer included, terms and documents are being preprocessed

---
+++ DocSearch/Controller.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DocSearch
+{
+    class Controller
+    {
+        PorterStemmer stemmer;
+        List<string> terms;
+        List<Tuple<string, List<string>>> documents;
+
+        public Controller()
+        {
+            stemmer = new PorterStemmer();
+            terms = new List<string>();
+            documents = new List<Tuple<string, List<string>>>();
+        }
+
+        private string CleanAndStemm(string word)
+        {
+            word = word.ToLower().Trim(new Char[] { ',', '.', ';', ':', '(', ')', '|', '&', '!', '-' });
+            word = stemmer.StemWord(word);
+            return word;
+        }
+
+        public string GetTermsPreview()
+        {
+            return String.Join("\n", terms);
+        }
+
+        public string GetDocumentsPreview()
+        {
+            var preview = "";
+            for (var i = 0; i < documents.Count; i++)
+            {
+                preview =
+                    preview
+                    + documents[i].Item1 + "\n\n"
+                    + String.Join("\n", documents[i].Item2)
+                    + "\n\n-----------\n\n";
+            }
+            return preview;
+        }
+
+        public void LoadTerms(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var word = sr.ReadLine();
+                    // applying stemming
+                    word = CleanAndStemm(word);
+                    if (!String.IsNullOrWhiteSpace(word))
+                    {
+                        terms.Add(word);
+                    }
+                }
+            }
+        }
+
+        public void LoadDocuments(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                var document = new List<string>();
+                var title = "";
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    if (line != "")
+                    {
+                        if (document.Count == 0)
+                        {
+                            title = line;
+                        }
+                        var words = line.Split(' ').ToList();
+                        // applying stemming
+                        foreach (var word in words)
+                        {
+                            var stemmedWord = CleanAndStemm(word);
+                            if (!String.IsNullOrWhiteSpace(stemmedWord))
+                            {
+                                document.Add(stemmedWord);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        documents.Add(new Tuple<string, List<string>>(title, document));
+                        document = new List<string>();
+                    }
+                }
+            }
+        }
+
+        public void Analyse()
+        {
+
+        }
+    }
+}
---
+++ DocSearch/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace DocSearch
+{
+    public partial class MainWindow : Window
+    {
+        private Controller controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            controller = new Controller();
+        }
+
+        private void TermsLoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                TermsTextBox.Text = ofd.SafeFileName;
+                controller.LoadTerms(ofd.FileName);
+                TermsPreviewButton.IsEnabled = true;
+            }
+        }
+
+        private void DocumentsLoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                DocumentsTextBox.Text = ofd.SafeFileName;
+                controller.LoadDocuments(ofd.FileName);
+                DocumentsPreviewButton.IsEnabled = true;
+            }
+        }
+
+        private void AnalyseButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(TermsTextBox.Text) && !String.IsNullOrWhiteSpace(DocumentsTextBox.Text))
+            {
+                controller.Analyse();
+            }
+            else
+            {
+                MessageBox.Show("Please load documents and terms first!");
+            }
+        }
+
+        private void TermsPreviewButton_Click(object sender, RoutedEventArgs e)
+        {
+            PreviewTextBox.Text = controller.GetTermsPreview();
+        }
+
+        private void DocumentsPreviewButton_Click(object sender, RoutedEventArgs e)
+        {
+            PreviewTextBox.Text = controller.GetDocumentsPreview();
+        }
+    }
+}
